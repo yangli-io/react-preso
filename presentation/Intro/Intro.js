a8d84b8ff6a236ed9bf40428765041607193c948
Yang Li <yangli@qantas.com.au>
2016-06-09 18:41:30 +1000

    first commit

---
+++ presentation/Intro/Intro.js
@@ -0,0 +1,39 @@
+import React, { Component } from "react";
+import {
+  Appear,
+  BlockQuote,
+  Cite,
+  CodePane,
+  Deck,
+  Fill,
+  Heading,
+  Image,
+  Layout,
+  Link,
+  ListItem,
+  List,
+  Markdown,
+  Quote,
+  Slide,
+  Spectacle,
+  Text
+} from "spectacle";
+
+export default class Intro extends Component {
+
+  render() {
+    return <div>
+      <Heading size={2} fit caps lineHeight={1} textColor="black">
+        Intro to React
+      </Heading>
+      <Heading size={1} fit caps>
+        A ReactJS Presentation With ES 6/ES Next
+      </Heading>
+      <Heading size={4} caps textColor="black">Yang Li</Heading>
+      <Link href="https://github.com/FormidableLabs/spectacle">
+        <Text bold caps textColor="tertiary">View on Github</Text>
+      </Link>
+
+    </div>
+  }
+}
